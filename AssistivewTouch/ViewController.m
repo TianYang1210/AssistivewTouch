a175c7028d7c6c3ab15f6a644d8013d4c3d48bec
chenxuhun <721782857@qq.com>
2016-03-03 12:47:28 +0800

    IPhone_AssitiveTouch
    高仿iPhone AssistiveTouch 可在屏幕随意移动，最终悬靠屏幕边缘，集成简单

--- AssistivewTouch/ViewController.m
+++ AssistivewTouch/ViewController.m
@@ -1,14 +1,14 @@
 //
 //  ViewController.m
-//  AssistivewTouch
+//  类似IPhone_AssitiveTouch圆点
 //
-//  Created by Chenxuhun on 16/3/3.
+//  Created by 陈绪混 on 16/3/2.
 //  Copyright © 2016年 Chenxuhun. All rights reserved.
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import "DTouchButton.h"
+@interface ViewController () <touchDelegate>
 
 @end
 
@@ -16,12 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor colorWithRed:0.337 green:0.698 blue:0.784 alpha:1.000];
+    
+    DTouchButton *touchBtn = [[DTouchButton alloc] initWithFrame:CGRectMake(0, 100, 58, 58)];
+    touchBtn.delegate = self;
+    touchBtn.userInteractionEnabled_DT = YES;
+    [self.view addSubview:touchBtn];
+   
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) touchDownDTButton
+{
+    NSLog(@"点击到AsstisTouchBtn");
 }
 
 @end
