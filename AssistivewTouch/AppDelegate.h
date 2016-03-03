80355e91961c31d0e614829843c44659a9a91cd9
chenxuhun <721782857@qq.com>
2016-03-03 12:37:57 +0800

    Initial Commit

---
+++ AssistivewTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssistivewTouch
+//
+//  Created by Chenxuhun on 16/3/3.
+//  Copyright © 2016年 Chenxuhun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
